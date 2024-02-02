f880ef6f46b70267eec120a75bf06f1d0370aa36
Xi Luo <xi.rossi.luo@gmail.com>
2024-02-02 10:57:28 -0600

    add comments

--- debugger/hw2_func.R
+++ debugger/hw2_func.R
@@ -1,14 +1,19 @@
-author <- "Stat Comp"
+author <- "Stat Comp" ## replace with your own name
 
 len_one <- function(x) {
     ## dummy solution, not working
+    ## replace with your own code
     return(5)
 }
 
 corcov_mat <- function(p = 10) {
+    ## dummy solution, not working
+    ## replace with your own code
     return(1)
 }
 
 roman_trans <- function(x) {
+    ## dummy solution, not working
+    ## replace with your own code
     return(5)
 }
